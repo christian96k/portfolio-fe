7a7e241befbeaebeca6b851e55aeb733cbc5df10
Christian Castro <christiancastro@MacBook-Pro-di-Christian.local>
2025-05-11 23:26:46 +0200

    fix: style

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -189,7 +189,7 @@ function Home() {
           description='I have a strong foundation in various programming languages and frameworks, including JavaScript, Python, Sql. I am also proficient in using tools such as Docker, Jenkins, and Git for version control and continuous integration.'
           rotatingText={images.map((item) => item.title.toUpperCase())}
         />
-        <div className="col-12 d-flex flex  border-gradient right py-5 col-12 backdrop-blur-5">
+        <div className="col-12 d-flex flex-column  border-gradient right py-5 col-12 backdrop-blur-5">
 
           <div className='home__about__infinite col-12 col-md-9 rounded-3 order-2 order-md-1' style={{ position: 'relative' }}>
             <InfiniteMenu items={images} activeItemChange={(index:number)=> handleActiveItemChange(index)}/>
